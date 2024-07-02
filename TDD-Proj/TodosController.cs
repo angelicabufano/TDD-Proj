1425404d02b3330e29aa506f35df3351024e3728
Angelica Bufano <abufano@tql.com>
2024-07-02 15:57:10 -0400

    added new methods

--- TDD-Proj/TodosController.cs
+++ TDD-Proj/TodosController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,13 +11,37 @@ public class TodosController {
     TodoDbContext _context = new TodoDbContext();
 
     public Todo AddTodo(Todo item) {
+        item.Id = 0;
         item.TextStatus = "Active";
-        item.DueDate.AddDays(7);
+        item.DueDate = new DateTime(2024,07,02).AddDays(7);
         item.TextPriority = "Medium";
         _context.Todos.Add(item);
+        _context.SaveChanges();
         return item;
+
     }
 
+    public IEnumerable<Todo> GetTodos() {
+        return _context.Todos.ToList();
 
+    }
 
+    public Todo UpdateTodos(int id, Todo updated) {
+        var item = _context.Todos.Find(id);
+        item.Description = updated.Description;
+        item.DueDate = updated.DueDate;
+        item.TextPriority = updated.TextPriority;
+        _context.SaveChanges();
+        return item;
+    }
+    public Todo GetTodoById(int id) {
+        var found = _context.Todos.Find(id);
+        return found;
+    }
+    public void DeleteTodo(int id) {
+
+        var deleted = _context.Todos.Find(id);
+        _context.Todos.Remove(deleted);
+        _context.SaveChanges();
+    }
 }
--- Todo-Test/UnitTest1.cs
+++ Todo-Test/UnitTest1.cs
@@ -5,19 +5,41 @@ using TDD_Proj;
 public class UnitTest1 {
     Todo item = default!;
     TodosController ctrl = default!;
-    TodoDbContext _context = default!;
+    
     public UnitTest1() { 
         item = new Todo();
         ctrl = new TodosController();
-        _context = new TodoDbContext();
     }
-
+    
     [Fact]
     public void Test1() {
-        Assert.True(_context.Todos.Any(i => i.Description == item.Description));
+        var todo1 = ctrl.AddTodo(item);
         Assert.Equal("Active", item.TextStatus);
-        Assert.Equal(item.DueDate.AddDays(7), item.DueDate);
         Assert.Equal("Medium", item.TextPriority);
+        Assert.True(todo1.Id == 4);
 
+    } 
+    [Fact]
+    public void Test2() {
+        var todos = ctrl.GetTodos();
+        Assert.Equal(todos.Count(), 3);
+    } 
+    [Fact]
+    public void Test3() {
+        item.Description = "brush teeth";
+        item.DueDate = new DateTime(2024,07,05);
+        item.TextPriority = "High";
+        var item2 = ctrl.UpdateTodos(4, item);
+        Assert.Equal(item2.Description, item.Description);
+    }
+    [Fact]
+    public void Test4() {
+        var todos = ctrl.GetTodoById(1);
+        Assert.NotNull(todos);
+    }
+    [Fact]
+    public void Test5() {
+        ctrl.DeleteTodo(2);
+        Assert.Null(ctrl.GetTodoById(2));
     }
 }
